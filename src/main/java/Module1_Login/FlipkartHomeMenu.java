dcbc45f696c0d4f6d872329b008256cf2bee628c
NihalBonde <nihalbonde03@gmail.com>
2024-12-18 13:16:30 +0530

    Nihal

---
+++ src/main/java/Module1_Login/FlipkartHomeMenu.java
@@ -0,0 +1,13 @@
+package Module1_Login;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FlipkartHomeMenu 
+{
+ public static void main(String[] args)
+ {
+	WebDriver driver= new ChromeDriver();
+	driver.get("https://www.google.com/");
+}
+}
---
+++ src/test/java/Module1_Login_Test/FlipkartLoginTest.java
@@ -0,0 +1,5 @@
+package Module1_Login_Test;
+
+public class FlipkartLoginTest {
+
+}
